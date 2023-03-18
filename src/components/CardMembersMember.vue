8fd403bd35f3da105a952c19042266ce384d7358
Valentin Vila <vilavalentin@gmail.com>
2023-03-19 00:35:16 +0100

    change place for email Copiado message & uppercase user bubble

--- src/components/CardMembersMember.vue
+++ src/components/CardMembersMember.vue
@@ -3,14 +3,14 @@
     <div className="items-center w-full grid grid-cols-2">
       <div className="flex items-center">
         <div className="flex rounded-full w-8 h-8 bg-violet-700 text-white text-xs items-center justify-center">
-          <p>{{ member.name[0] }}{{ member.surname[0] }}</p>
+          <p>{{ member.name[0].toUpperCase() }}{{ member.surname[0].toUpperCase() }}</p>
         </div>
         <div className="flex flex-col">
           <div className="flex ">
             <p class="text-[#1C2E3D] font-medium text-lg pl-4">{{ member.name }} {{ member.surname }}</p>
             <button v-on:click="copyEmailToClipboard" class="flex items-center">
               <img class="pl-2 w-6 h-6" src="@/assets/email.svg" />
-              <p v-if="copyToClipboard" class="text-[#62948F] font-medium text-xs pl-1">Copiado</p>
+              <p v-if="copyToClipboard" class="mb-10 ml-[-12px] absolute text-[#62948F] font-medium text-xs pl-1">Copiado</p>
             </button>
           </div>
           <p v-if="member.teams" class="text-[#8D8B96] font-medium text-lg pl-4">{{ member.teams[0].team_name }}</p>
